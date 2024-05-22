d7064d170001bb516602432af8cc91c19d41d0c8
YeRegie <gallenaregie23@gmail.com>
2024-05-22 16:41:11 +0800

    Update model.py

--- model.py
+++ model.py
@@ -3,7 +3,6 @@
 
 # In[1]:
 
-
 import torch 
 import torch.nn as nn
 
@@ -11,22 +10,22 @@ import torch.nn as nn
 # In[4]:
 
 
-class nlp(nn.Module):
-    def _init_(self, input_size, hidden_size, num_classes):
-        super (nlp, self)._init_()
-        self.l1 =nn.linear(input_size, hidden_size)
-        self.l2 =nn.linear(hidden_size, hidden_size)
-        self.l3 =nn.linear(input_size, num_classes)
-        
+class NeuralNet (nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        self.l1 = nn.Linear(input_size, hidden_size) 
+        self.l2 = nn.Linear(hidden_size, hidden_size) 
+        self.l3 = nn.Linear(hidden_size, num_classes)
+        self.relu = nn.ReLU()
+    
     def forward(self, x):
         out = self.l1(x)
         out = self.relu(out)
         out = self.l2(out)
         out = self.relu(out)
         out = self.l3(out)
-        #no activtion and no softmax
+        # no activation and no softmax at the end
         return out
-
 
 # In[ ]:
 
